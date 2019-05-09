f3644b1f5094f9bb9eb6fb51e9aff7417a061ad0
core.editor <andreeavillatte@gmail.com>
2019-05-09 16:56:54 +0200

    après corrections

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('myapp', []);
-
-app.controller('myCtrl', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
-  $scope.item = $filter('date')('2017-01-23', "dd/MM/yyyy");
-}]);
-
